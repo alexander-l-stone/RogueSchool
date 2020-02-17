925da64c4db5e64413b9ea3a4ac9fa3218bcb16a
Alexander Stone <aloecend@gmail.com>
2020-02-17 07:38:16 -0500

    More scaffolding to set up basic tdl.

--- game/engine.py
+++ game/engine.py
@@ -1,6 +1,51 @@
+import tdl
+
 class GameEngine:
-    def __init__(self):
-        self.action_queue = []
+    def __init__(self, console, main_window, screen_width, screen_height):
+        self.action_queue = [] #These are all Action Objects
         self.areas = {}
         self.current_area = None
-        
+        self.console = console
+        self.main_window = main_window
+        self.SCREEN_HEIGHT = screen_height
+        self.SCREEN_WIDTH = screen_width
+    
+    def game_loop(self):
+        while not tdl.event.is_window_closed():
+            self.pop()
+            self.draw()
+
+            for event in tdl.event.get():
+                if event.type == 'KEYDOWN' and event.keychar != 'TEXT':
+                    key_x, key_y = self.MOVEMENT_KEYS.get(
+                        event.keychar, (0, 0))
+                    action = self.ACTION_KEYS.get(event.keychar, (0, 0))
+                    self.player.move(self.currentArea, key_x, key_y)
+
+    def pop(self):
+        if len(self.action_queue) < 1:
+            return
+        if self.action_queue[0].time_left <= 0:
+            self.action_queue[0].resolve()
+            self.action_queue.pop(0)
+        else:
+            for action in self.action_queue:
+                action.time_left -= 1
+    
+    def push(self, action):
+        self.action_queue.append(action)
+    
+    def draw(self):
+        self.current_area.draw(self.console)
+        self.main_window.blit(self.console, 0, 0, self.SCREEN_WIDTH, self.SCREEN_HEIGHT, 0, 0)
+        tdl.flush()
+
+#This is an interface. NEVER EVER USE
+# TODO: See if this needs additional stuff
+class Action:
+    def __init__(self, time_left):
+        self.time_left = time_left
+    
+    #This needs to be overwritten
+    def resolve(self):
+        return
--- main.py
+++ main.py
@@ -1,2 +1,34 @@
 from game.engine import GameEngine
+from game.entity.entity import Entity
+from game.area.area import Area
+import tdl
+
+
+def main():
+    #set up the console
+    SCREEN_WIDTH = 80
+    SCREEN_HEIGHT = 50
+    font_path = 'arial12x12.png'
+    tdl.set_font(font_path, greyscale=True, altLayout=True)
+    title = "Rogue School"
+    fullscreen = False
+    console = tdl.Console(SCREEN_WIDTH, SCREEN_HEIGHT)
+    main_window = tdl.init(SCREEN_WIDTH, SCREEN_HEIGHT, title=title, fullscreen=fullscreen)
+
+    player = Entity(int(SCREEN_WIDTH/2), int(SCREEN_HEIGHT/2), '@', (255,0,0))
+
+    start_area = Area(0, 0, 0, 'empty')
+
+    start_area.entity_dict[(player.x, player.y)] = player
+    player.area = start_area
+
+    game = GameEngine(console, main_window, SCREEN_WIDTH, SCREEN_HEIGHT)
+    game.current_area = start_area
+    game.game_loop()
+    
+
+if __name__ == '__main__':
+    main()
+
+
 
